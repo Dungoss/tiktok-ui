2613b915f90af283be926c0d44a7fbc6b6c6150c
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-08-03 11:19:38 +0700

    Cấu hình router và xây dựng cơ chế tải Layout cho dự án TikTok

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+//Layouts
+import { HeaderOnly } from '../components/Layout'
+
+
+import Home from '../page/Home';
+import Following from '../page/Following';
+import Profile from '../page/Profile';
+import Upload from '../page/Upload';
+import Search from '../page/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+]
+
+const privateRoutes = [
+
+]
+
+
+export { publicRoutes, privateRoutes}
