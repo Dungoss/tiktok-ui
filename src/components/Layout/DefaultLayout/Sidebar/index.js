783e735964ff950d9ba8fe78b406fc0fe1bc907b
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-08-03 13:48:27 +0700

    Dựng khung DefaultLayout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,12 @@
+import className from 'classnames/bind';
+import styles from './Sidebar.module.scss'
+
+const cx = className.bind(styles)
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return <aside className={cx('wrapper')}>
+        <h2>Sidebar</h2>
+    </aside>;
 }
 
 export default Sidebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,18 @@
+
+import className from 'classnames/bind';
 import Header from "../components/Header";
 import Sidebar from "./Sidebar";
+import styles from './DefaultLayout.module.scss';
+
+const cx = className.bind(styles)
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx("container")}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx("content")}>{children}</div>
             </div>
         </div>
     )
