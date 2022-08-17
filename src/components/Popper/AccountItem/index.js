7c6429338baa0d7ddebe882b65f1c1d6f4584a69
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-08-17 10:04:25 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #2

---
+++ src/components/Popper/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from '../AccountItem/AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <img 
+                className={cx('avatar')} 
+                src ="https://cdn2.vectorstock.com/i/1000x1000/53/36/nice-astronaut-with-equipment-to-kawaii-avatar-vector-15475336.jpg" 
+                alt="Hoaa" 
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}></FontAwesomeIcon>
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
